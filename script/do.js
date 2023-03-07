26d1bf0b875073040cdaa297ddb55030870eb00d
hauretax <huggotrico@protonmail.ch>
2023-03-07 14:40:44 +0100

    add timer

--- script/do.js
+++ script/do.js
@@ -1,20 +1,12 @@
 
-// let test = new Circle({ y: 0, x: 0, size: 30 })
-// let test2 = new Circle({})
 
 let nbTouch = 0;
 let click = 0;
 let nbCible = 2;
 let startTime;
-
-
 let canvaObj = new Canvashandler()
 
-// testhandl.addCircle(test);
-
-// let test3 = new Circle({})
-// console.log(test3.setRandomPosition())
-
+//initializing 
 function printCircles() {
     for (let i = 0; i < nbCible; i++) {
         let c = new Circle({});
@@ -23,26 +15,15 @@ function printCircles() {
     }
     canvaObj.print();
 }
-
 printCircles()
 
-
-// function superfn() {
-//     printCircles()
-//     setTimeout(superfn, 1000);
-// }
-
-// superfn()
-
+//onclick do that
 function clickObject(event) {
     if (!click) {
         startTime = Date.now();
     }
     click++;
     event.stopPropagation()
-    //console.log(event)
-    // canvaObj.clearOneObject(2)
-    // canvaObj.print();
     nbTouch += canvaObj.deletClickedObject({ x: event.offsetX, y: event.offsetY })
     canvaObj.print();
     console.log(nbTouch)
@@ -51,5 +32,4 @@ function clickObject(event) {
         console.log('u used:', totalTime / 1000)
     }
 }
-
 canvas.addEventListener('click', clickObject, false);
